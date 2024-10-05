996585d95914dac9ca1d690ba8caa5f561ea88fa
Karn Wong <karn@karnwong.me>
2024-10-05 18:39:40 +0700

    plugin: add nathangrigg/vim-beancount

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,5 +1,6 @@
 local plugins = {
     { "wakatime/vim-wakatime", lazy = false },
+    { "nathangrigg/vim-beancount", lazy = true },
     -- this opts will extend the default opts
     {
         "nvim-treesitter/nvim-treesitter",
