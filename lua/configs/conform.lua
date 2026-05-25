76e1ae250c492d391e174476c168e199306670ca
Karn Wong <karn@karnwong.me>
2026-05-25 17:29:55 +0700

    disable format on save

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -13,11 +13,11 @@ local options = {
     yaml = { "prettier" },
   },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+--   format_on_save = {
+--     -- These options will be passed to conform.format()
+--     timeout_ms = 500,
+--     lsp_fallback = true,
+--   },
 }
 
 return options
